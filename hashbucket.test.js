4a09bf27b232326ed9f96cfd026951881fb5ee40
Brandon Perard <Brandon.Perard@Gmail.com>
2023-08-16 23:29:20 -0700

    ADD removeItem() tests

--- hashbucket.test.js
+++ hashbucket.test.js
@@ -10,10 +10,9 @@ describe('Hashbucket', () => {
     expect(newHashbucket.size).toEqual(size);
   });
 
-  const size = 20;
-  const testBucket = new Hashbucket(size);
-
   it('Returns a hashkey in range 0 to this.size - 1', () => {
+    const size = 20;
+    const testBucket = new Hashbucket(size);
     const hashedKey = testBucket.hashKey('this is my key');
 
     expect(hashedKey).toBeGreaterThanOrEqual(0);
@@ -21,13 +20,38 @@ describe('Hashbucket', () => {
   });
 
   it('Adds item to bucket of hashed index', () => {
+    const size = 20;
+    const testBucket = new Hashbucket(size);
     const item = {
       key: 'this is my key',
       value: 42,
     };
 
-    testBucket.addItem(item.key, item);
+    testBucket.addItem(item);
     const currentBucket = testBucket.buckets[testBucket.hashKey(item.key)];
+
     expect(currentBucket).toEqual([item]);
+  });
+
+  it('Removes item from bucket of hashed index', () => {
+    const singleBucket = new Hashbucket(1);
+    const item0 = {
+      key: 'this is my key',
+      value: 42,
+    };
+    const item1 = {
+      key: 'this is also my key',
+      value: 101,
+    };
+
+    singleBucket.addItem(item0);
+    const currentBucket = singleBucket.buckets[0];
+    expect(currentBucket).toEqual([item0]);
+
+    singleBucket.addItem(item1);
+    expect(currentBucket).toEqual([item0, item1]);
+
+    singleBucket.removeItem(item0.key);
+    expect(currentBucket).toEqual([item1]);
   });
 });
